2fcc23f8ac5787b849b3ec22d68d6062fbf26a76
Deepanshu kumar <anshuk6469@gmail.com>
2023-09-19 23:07:08 +0530

    updated

--- integration-test.sh
+++ integration-test.sh
@@ -7,12 +7,12 @@ sleep 5s
 PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
 echo $PORT
-echo $applicationURL:${PORT}${applicationURI}
+echo $applicationURL:${PORT}
 
 if [[ ! -z "$PORT" ]];
 then
 
-    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT)
 
     if [[ "$http_code" == 200 ]];
         then
