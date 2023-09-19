cb281e33ac57896c362fe080894fe388697fde2c
Deepanshu kumar <anshuk6469@gmail.com>
2023-09-19 22:56:09 +0530

    changed

--- integration-test.sh
+++ integration-test.sh
@@ -7,21 +7,12 @@ sleep 5s
 PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
 echo $PORT
-echo $applicationURL:$PORT/$applicationURI
+echo $applicationURL:${PORT}${applicationURI}
 
 if [[ ! -z "$PORT" ]];
 then
 
-    response=$(curl -s $applicationURL:$PORT$applicationURI)
     http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
-
-    if [[ "$response" == 100 ]];
-        then
-            echo "Increment Test Passed"
-        else
-            echo "Increment Test Failed"
-            exit 1;
-    fi;
 
     if [[ "$http_code" == 200 ]];
         then
