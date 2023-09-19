03736808c8217b41a515a3fc4c5ff7049150ff3f
Deepanshu kumar <anshuk6469@gmail.com>
2023-09-19 18:20:28 +0530

    Mutation resloved

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -36,20 +36,25 @@ public class NumericApplicationTests {
 
     @Test
     public void smallerThanOrEqualToFiftyMessage() throws Exception {
-        this.mockMvc.perform(get("/compare/49")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/compare/50")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Smaller than or equal to 50"));
     }
-
+    // Change the above value to 50.Earlier it was 49.So it can survive mutation testing.
     @Test
     public void greaterThanFiftyMessage() throws Exception {
         this.mockMvc.perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Greater than 50"));
     }
-    
+//    Below code doesn't survived mutation test
+//    @Test
+//    public void welcomeMessage() throws Exception {
+//         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+//    }
+
     @Test
     public void welcomeMessage() throws Exception {
-         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("Kubernetes DevSecOps"));
     }
-    
 
 }
